96e625acc7e5b364f28230b7dfa4db31bd7523cf
philips <philencripted@gmail.com>
2019-11-25 16:30:17 +0100

    added the share was feature

--- app/src/main/java/tech/phixlab/nota/NoteDetail.java
+++ app/src/main/java/tech/phixlab/nota/NoteDetail.java
@@ -1,15 +1,14 @@
 package tech.phixlab.nota;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
-import android.widget.TextView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class NoteDetail extends AppCompatActivity {
 
@@ -51,6 +50,20 @@ public class NoteDetail extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String shareText = "";
+
+                if(!noteTitle.equals("") && !noteBody.equals("") && !noteDateTime.equals("")){
+                    shareText = noteTitle
+                            + " \n "+ noteDateTime
+                            + " \n \n \n " + noteBody;
+                }
+
+                //Convert file path into Uri for sharing
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                intent.setType("text/*");
+                intent.putExtra(android.content.Intent.EXTRA_TEXT,shareText  );
+                startActivity(Intent.createChooser(intent, "Share Via"));
 
             }
         });
